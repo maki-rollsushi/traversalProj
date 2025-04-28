03ba489d8319d30c209bdad5079b5e3d291c904f
makiroll.mark <delacruzmacky39@gmail.com>
2025-04-28 14:50:44 +0800

    pushed the command line arguments

--- main.c
+++ main.c
@@ -22,14 +22,15 @@ void printPostfix(binTree* root);
 void printPrefix(binTree* root);
 void printInfix(binTree* root);  
 
-void printHelp();
-void printGuide();
+void help();
+void guide();
 
 
 
 //takes an input from the user.
 int main(int argc, char* argv[]) {
-    if (argc == 2) { // Only 2 arguments, might be --help or --guide
+    //Only one input possibly -h, --help or --guide
+    if (argc == 2) {
         if (strcmp(argv[1], "-h") == 0 || strcmp(argv[1], "--help") == 0) {
             help();
             return 0;
@@ -39,12 +40,13 @@ int main(int argc, char* argv[]) {
             return 0;
         }
         else {
-            printf("Error: Unknown option. Use -h or --help.\n");
+            printf("Error: Unknown option. Use -h or --help or --guide for more details.\n ");
             return 1;
         }
     }
     else if (argc == 6) { 
         // Expected input: ./program "expression" --from X --to Y
+        
         char* expression = argv[1];
         char* fromFormat = NULL;
         char* toFormat = NULL;
@@ -54,7 +56,7 @@ int main(int argc, char* argv[]) {
             fromFormat = argv[3];
         }
         else {
-            printf("Error: Missing or incorrect --from argument.\n");
+            printf("Error: Missing or incorrect --from argument. Input is \'%s\' it must be --from \n", argv[2]);
             return 1;
         }
 
@@ -62,24 +64,28 @@ int main(int argc, char* argv[]) {
             toFormat = argv[5];
         }
         else {
-            printf("Error: Missing or incorrect --to argument.\n");
+            printf("Error: Missing or incorrect --to argument. Input is \'%s\' it must be --to\n", argv[5]);
             return 1;
         }
-
+        
         // Build the expression tree
         binTree* root = NULL;
 
         if (strcmp(fromFormat, "postfix") == 0) {
-            root = buildFromPostfix(expression);
+            printf("Create tree from postfix.\n");
+            //root = buildFromPostfix(expression);
         }
         else if (strcmp(fromFormat, "prefix") == 0) {
-            root = buildFromPrefix(expression);
+            //root = buildFromPrefix(expression);
+            printf("Create tree from prefix.\n");
+
         }
         else if (strcmp(fromFormat, "infix") == 0) {
-            root = buildFromInfix(expression);
+            //root = buildFromInfix(expression);
+            printf("Create tree from infix.\n");
         }
         else {
-            printf("Error: Invalid format for --from. Must be infix, prefix, or postfix.\n");
+            printf("Error: Invalid format specifier \'%s\' for --from. Must be infix, prefix, or postfix.\n", fromFormat);
             return 1;
         }
 
@@ -102,7 +108,7 @@ int main(int argc, char* argv[]) {
             printf("\n");
         }
         else {
-            printf("Error: Invalid format for --to. Must be infix, prefix, or postfix.\n");
+            printf("Error: Invalid format specifier \'%s\' for --to. Must be infix, prefix, or postfix.\n", toFormat);
             return 1;
         }
     }
@@ -113,6 +119,14 @@ int main(int argc, char* argv[]) {
 
     return 0;
 }
+
+void help(){
+    //code here
+}
+void guide(){
+    //code here
+}
+
 
 //return value is the root node
 binTree* buildPostfix(const char* expression){
